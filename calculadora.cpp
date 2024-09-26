5518de717ed8e237bea0b63875a7e9e5d7ee9661
LeoMessiReal <juancamaya0812@gmail.com>
2024-09-26 09:26:30 -0500

    Suma y codigo arreglado

--- calculadora.cpp
+++ calculadora.cpp
@@ -3,12 +3,24 @@ using namespace std;
 
 int main()
 {
-    int a,b,c;
+    int a,b,c,operacion;
+
+    cout<<"Ingrese la operacion que desea hacer: ";
+    cout <<"\n 1. Sumas \n 2. Restas";
+    cin>>operacion;
+    if (operacion ==1 ) {
+    cout<<"ingrese los numeros para sumar: ";
+    cin>>a;
+    cin>>b;
+    c= a+b;
+    cout<<"El resultado de la suma es: "<< c;
+    }
+    if (operacion ==2) {
     cout<<"ingrese dos numeros a restar";
     cin>>a;
     cin>>b;
     c= a-b;
-cout<<"El resultado de su resta es: "<<c;
-
+    cout<<"El resultado de su resta es: " << c;
+    }
     return 0;
 }
